326915c09ac924f0263b335a725895fc12c0ddd4
wangliang <1991wangliang@gmail.com>
2020-04-12 21:15:22 +0800

    排序问题

--- src/main/java/com/codingapi/maven/cole/ColaMojo.java
+++ src/main/java/com/codingapi/maven/cole/ColaMojo.java
@@ -71,7 +71,7 @@ public class ColaMojo extends AbstractMojo {
         Collections.sort(markdowns, new Comparator<Markdown>() {
             @Override
             public int compare(Markdown o1, Markdown o2) {
-                return o1.getName().compareTo(o2.getName());
+                return o2.getName().compareTo(o1.getName());
             }
         });
     }
